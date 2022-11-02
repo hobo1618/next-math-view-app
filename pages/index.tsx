87c6864c01f4064024573e72d149a23493034796
Hobo Spaceship <hobdenw@gmail.com>
2022-11-02 14:59:04 +0100

    remove useless imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,4 @@
 import dynamic from "next/dynamic";
-import { useRef, useEffect, useState } from "react";
-// import MathView, { MathViewRef } from "react-math-view";
-// import { makeSharedVirtualKeyboard } from "mathlive";
-import { HIGH_SCHOOL_KEYBOARD, HIGH_SCHOOL_KEYBOARD_LAYER } from "../keyboard";
-
-
-// makeSharedVirtualKeyboard();
 
 const App = dynamic(() => import('../components/MathViewNoSSR'), {
   ssr: false
